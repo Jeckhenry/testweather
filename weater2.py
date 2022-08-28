ce2f8d334c3172da907c65fbb244db966beda03c
Jeckhenry <571977485@qq.com>
2022-08-28 16:54:10 +0800

    adddd

---
+++ weater2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+#coding=utf-8
+
+import requests, json
+import os
+from datetime import datetime, timezone
+from datetime import date
+
+SCKEY=os.environ.get('SCKEY2') ##Server酱推送KEY
+def get_iciba_everyday():
+    icbapi = 'http://open.iciba.com/dsapi/'
+    eed = requests.get(icbapi)
+    bee = eed.json()  #返回的数据
+    english = bee['content']
+    zh_CN = bee['note']
+    strs = '【奇怪的知识】\n' + english + '\n' + zh_CN
+    return strs
+
+def ServerPush(info): #Server酱推送
+    api = "https://sc.ftqq.com/{}.send".format(SCKEY)
+    title = u"天气推送"
+    content = info.replace('\n','\n\n')
+    data = {
+        "text": title,
+        "desp": content
+    }
+    print(content)
+    requests.post(api, data=data)
+def main():
+    try:
+        api = 'http://t.weather.itboy.net/api/weather/city/'             #API地址，必须配合城市代码使用
+        city_code = '101010100'   #进入https://where.heweather.com/index.html查询你的城市代码
+        tqurl = api + city_code
+        response = requests.get(tqurl)
+        d = response.json()         #将数据以json形式返回，这个d就是返回的json数据
+        if(d['status'] == 200):     #当返回状态码为200，输出天气状况
+            parent = d["cityInfo"]["parent"] #省
+            city = d["cityInfo"]["city"] #市
+            update_time = d["time"] #更新时间
+            dates = d["data"]["forecast"][0]["ymd"] #日期
+            week = d["data"]["forecast"][0]["week"] #星期
+            weather_type = d["data"]["forecast"][0]["type"] # 天气
+            wendu_high = d["data"]["forecast"][0]["high"] #最高温度
+            wendu_low = d["data"]["forecast"][0]["low"] #最低温度
+            shidu = d["data"]["shidu"] #湿度
+            pm25 = str(d["data"]["pm25"]) #PM2.5
+            pm10 = str(d["data"]["pm10"]) #PM10
+            quality = d["data"]["quality"] #天气质量
+            fx = d["data"]["forecast"][0]["fx"] #风向
+            fl = d["data"]["forecast"][0]["fl"] #风力
+            ganmao = d["data"]["ganmao"] #感冒指数
+            tips = d["data"]["forecast"][0]["notice"] #温馨提示
+            # 天气提示内容 
+            nowDay = datetime.now(timezone.utc)
+            endDay = date(nowDay.year, nowDay.month, nowDay.day)
+            startDay = date(2019, 3, 27)
+            dayLen = endDay - startDay
+            print(dayLen.days)
+            tdwt = "宝贝，今天是我们在一起的第" + str(dayLen.days) + "天，希望宝贝开开心心的，爱你😘\n" + "【今日份天气】\n城市： " + parent + city + \
+                   "\n日期： " + dates + "\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
+                    shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
+                   "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time + "\n✁-----------------------------------------\n" + get_iciba_everyday()
+            # print(tdwt)
+            # requests.post(cpurl,tdwt.encode('utf-8'))         #把天气数据转换成UTF-8格式，不然要报错。
+            ServerPush(tdwt)
+    except Exception as r:
+        error = '【出现错误】\n　　今日天气推送错误，请检查服务或网络状态！'
+        print(error)
+        print(r)
+
+if __name__ == '__main__':
+    main()
+    
